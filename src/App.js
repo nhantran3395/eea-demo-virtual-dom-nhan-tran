79d8cf4a2cb6de4cf54e16adb555c36266762b05
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2021-05-29 10:51:59 +0700

    refactor: remove unneded src files and update import statement in other files accordingly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -13,9 +11,8 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          rel="noopener noreferrer">
+          Hello ! This is Saturday yo !
         </a>
       </header>
     </div>
