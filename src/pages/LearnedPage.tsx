b4d311863832e4b6f045e922f51cae689fde37e6
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2021-05-29 21:15:37 +0700

    feat: add info on learned page

--- src/pages/LearnedPage.tsx
+++ src/pages/LearnedPage.tsx
@@ -1,5 +1,15 @@
 function LearnedPage() {
-  return <h1>Learned</h1>;
+  return (
+    <div>
+      <h1>Learned</h1>
+      <p>Learn from this 2 weeks</p>
+      <ul>
+        <li>HTML & CSS</li>
+        <li>JavaScript on Browser (DOM, Event, Fetch, etc.)</li>
+        <li>Basics of React</li>
+      </ul>
+    </div>
+  );
 }
 
 export default LearnedPage;
