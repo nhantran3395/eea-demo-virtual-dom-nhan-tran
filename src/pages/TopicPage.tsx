064e3b67cf48f6d1244e1f362d002aa1f187de8f
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2021-05-29 21:37:37 +0700

    feat: add contents on topic page

--- src/pages/TopicPage.tsx
+++ src/pages/TopicPage.tsx
@@ -1,5 +1,14 @@
+import './TopicPage.css';
+
 function TopicPage() {
-  return <h1>Topic</h1>;
+  return (
+    <div>
+      <h1>Topic</h1>
+      <h1 id="highlighted-big-heading">The Virtual Dom</h1>
+      <p>Came accross during React study</p>
+      <p>One of the reason that make React performant</p>
+    </div>
+  );
 }
 
 export default TopicPage;
