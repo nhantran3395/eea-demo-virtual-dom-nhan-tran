b84dcfaf894492115ca0428f9689c3ddf3061a2d
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2021-06-01 20:53:07 +0700

    feat: add new contents on DOM page

--- src/pages/DomPage.tsx
+++ src/pages/DomPage.tsx
@@ -1,13 +1,35 @@
-import domConcept from '../dom_concept.jpg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import domConcept from '../imgs/dom_concept.jpg';
+import domExample from '../imgs/dom_example.jpg';
 import './DomPage.css';
 
 function DomPage() {
   return (
     <div>
       <h1>DOM</h1>
-      <div id="dom-image-container">
-        <img src={domConcept} alt="dom concept" id="dom-image" />
-      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/dom">concept</Link>
+          </li>
+          <li>
+            <Link to="/dom/example">example</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/dom" exact>
+          <div className="dom-image-container">
+            <img src={domConcept} alt="dom concept" className="dom-image" />
+          </div>
+        </Route>
+        <Route path="/dom/example" exact>
+          <div className="dom-image-container">
+            <img src={domExample} alt="dom example" className="dom-image" />
+          </div>
+        </Route>
+      </Switch>
     </div>
   );
 }
