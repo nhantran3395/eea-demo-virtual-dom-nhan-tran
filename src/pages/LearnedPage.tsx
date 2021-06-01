1c800a6b3e4f6b9f53bb0ed431ea0596dbfa5b30
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2021-06-01 22:17:26 +0700

    feat: make 3 columns contents and add svg on learned page

--- src/pages/LearnedPage.tsx
+++ src/pages/LearnedPage.tsx
@@ -1,12 +1,60 @@
+import htmlCssIcon from '../imgs/html_css.svg';
+import jsIcon from '../imgs/js.svg';
+import reactIcon from '../imgs/react.svg';
+
 function LearnedPage() {
   return (
     <div>
       <h1>Learned</h1>
       <p>Learn from this 2 weeks</p>
-      <ul>
-        <li>HTML & CSS</li>
-        <li>JavaScript on Browser (DOM, Event, Fetch, etc.)</li>
-        <li>Basics of React</li>
+      <ul
+        style={{ display: 'flex', flexDirection: 'row', marginTop: '30px  ' }}>
+        <li
+          style={{
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <img
+            src={htmlCssIcon}
+            alt=""
+            style={{ width: '200px', marginBottom: '45px' }}
+          />
+          <p>HTML & CSS</p>
+        </li>
+        <li
+          style={{
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <img
+            src={jsIcon}
+            alt=""
+            style={{ width: '200px', marginBottom: '45px' }}
+          />
+          <p>JavaScript on Browser</p>
+          <p>(DOM, Event, Fetch, etc.)</p>
+        </li>
+        <li
+          style={{
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <img
+            src={reactIcon}
+            alt=""
+            style={{ width: '280px', marginBottom: '45px' }}
+          />
+          Basics of React
+        </li>
       </ul>
     </div>
   );
