5a010e64e20f7aaaa0ca5923859146af8f81ee2b
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2021-06-01 10:50:15 +0700

    feat: add contents on dom page

--- src/pages/DomPage.tsx
+++ src/pages/DomPage.tsx
@@ -1,5 +1,15 @@
+import domConcept from '../dom_concept.jpg';
+import './DomPage.css';
+
 function DomPage() {
-  return <h1>DOM</h1>;
+  return (
+    <div>
+      <h1>DOM</h1>
+      <div id="dom-image-container">
+        <img src={domConcept} alt="dom concept" id="dom-image" />
+      </div>
+    </div>
+  );
 }
 
 export default DomPage;
