bcded8afdc1e22aa6a99aacab4e02a55c61797d9
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2021-06-01 10:49:35 +0700

    style: edit text display on Topic page

--- src/pages/TopicPage.tsx
+++ src/pages/TopicPage.tsx
@@ -6,7 +6,7 @@ function TopicPage() {
       <h1>Topic</h1>
       <h1 id="highlighted-big-heading">The Virtual Dom</h1>
       <p>Came accross during React study</p>
-      <p>One of the reason that make React performant</p>
+      <p>One of the reason that make React fast</p>
     </div>
   );
 }
