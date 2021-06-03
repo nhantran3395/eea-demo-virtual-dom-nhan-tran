06b2b58dd3a93a8ee0816fa7d3700223332df0c1
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2021-06-03 21:36:28 +0700

    feat: add more icons on problem page

--- src/pages/ProblemPage.tsx
+++ src/pages/ProblemPage.tsx
@@ -1,15 +1,62 @@
+import reUpdateIcon from '../imgs/reupdate.svg';
+import slowIcon from '../imgs/slow.svg';
+import efficientlyIcon from '../imgs/efficiently.svg';
+
 function ProblemPage() {
   return (
     <div>
       <h1>Problems</h1>
-      <ul>
-        <li>
-          Each change to the DOM will cause the browser to paint the content
-          again
+      <br />
+      <ul
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          marginTop: '30px  ',
+        }}>
+        <li
+          style={{
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <img
+            src={reUpdateIcon}
+            alt=""
+            style={{ width: '200px', marginBottom: '45px' }}
+          />
+          <p>Changes to the DOM trigger browser to rerender contents</p>
         </li>
-        <li>These processes are slow</li>
-        <li>
-          Making changes to the DOM inefficiently will cause drop in performance
+        <li
+          style={{
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <img
+            src={slowIcon}
+            alt=""
+            style={{ width: '200px', marginBottom: '45px' }}
+          />
+          <p>Re-render are slow</p>
+        </li>
+        <li
+          style={{
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <img
+            src={efficientlyIcon}
+            alt=""
+            style={{ width: '200px', marginBottom: '45px' }}
+          />
+          <p>Would want to manage updating and rerendering efficiently</p>
         </li>
       </ul>
     </div>
