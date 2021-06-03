44d7b5eb3e9e057ce183f62161a8c7a09613e577
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2021-06-03 21:16:41 +0700

    feat: add agenda as inner page

--- src/pages/TopicPage.tsx
+++ src/pages/TopicPage.tsx
@@ -1,4 +1,5 @@
 import './TopicPage.css';
+import { Switch, Route, Link } from 'react-router-dom';
 import studyIcon from '../imgs/study.svg';
 import interestingIdeaIcon from '../imgs/interesting_idea.svg';
 import fastIcon from '../imgs/fast.svg';
@@ -9,56 +10,88 @@ function TopicPage() {
       <h1>Topic</h1>
       <h1 id="highlighted-big-heading">The Virtual Dom</h1>
       <br />
-      <ul
-        style={{ display: 'flex', flexDirection: 'row', marginTop: '30px  ' }}>
-        <li
-          style={{
-            flex: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <img
-            src={studyIcon}
-            alt=""
-            style={{ width: '200px', marginBottom: '45px' }}
-          />
-          <p>Came accross during study</p>
-        </li>
-        <li
-          style={{
-            flex: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <img
-            src={interestingIdeaIcon}
-            alt=""
-            style={{ width: '200px', marginBottom: '45px' }}
-          />
-          <p>Interesting idea that</p>
-          <p>React introduced</p>
-        </li>
-        <li
-          style={{
-            flex: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <img
-            src={fastIcon}
-            alt=""
-            style={{ width: '200px', marginBottom: '45px' }}
-          />
 
-          <p>Reason that make React fast</p>
-        </li>
-      </ul>
+      <Switch>
+        <Route path="/topic/agenda" exact>
+          <br />
+          <ol>
+            <li>What is DOM?</li>
+            <li>Some problems when working with the DOM</li>
+            <li>What is Virtual DOM?</li>
+            <li>How we benefit from Virtual DOM</li>
+          </ol>
+        </Route>
+
+        <Route path="/topic" exact>
+          <ul
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              marginTop: '30px  ',
+            }}>
+            <li
+              style={{
+                flex: 1,
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <img
+                src={studyIcon}
+                alt=""
+                style={{ width: '200px', marginBottom: '45px' }}
+              />
+              <p>Came accross during study</p>
+            </li>
+            <li
+              style={{
+                flex: 1,
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <img
+                src={interestingIdeaIcon}
+                alt=""
+                style={{ width: '200px', marginBottom: '45px' }}
+              />
+              <p>Interesting idea that</p>
+              <p>React introduced</p>
+            </li>
+            <li
+              style={{
+                flex: 1,
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <img
+                src={fastIcon}
+                alt=""
+                style={{ width: '200px', marginBottom: '45px' }}
+              />
+              <p>Reason that make React fast</p>
+            </li>
+          </ul>
+        </Route>
+      </Switch>
+
+      <br />
+      <br />
+
+      <nav className="page-inner-navigation">
+        <ul>
+          <li>
+            <Link to="/topic">Topic</Link>
+          </li>
+          <li>
+            <Link to="/topic/agenda">Agenda</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
